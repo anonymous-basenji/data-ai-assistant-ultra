3eb2f96dd65c05e5cfbfca77dd2e35ba8e821ee7
anonymous-basenji <andrew.v.sklyarov@gmail.com>
2025-07-26 18:30:10 -0400

    Add chat retrieval to SideMenu.jsx

--- src/SideMenu.jsx
+++ src/SideMenu.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import { auth } from './firebase';
+import { useEffect, useState } from 'react';
+import { auth, db } from './firebase';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
 import './SideMenu.css';
 
 function SideMenu() {
     const [chats, setChats] = useState([]);
-    const chatQuery = query(collection('users', ))
+
+    useEffect(() => {
+        if(auth.currentUser) {
+            const q = query(collection(db, 'users', auth.currentUser.uid, 'chats'), orderBy('createdAt', 'desc'));
+
+            const unsubscribe = onSnapshot(q, (querySnapshot) => {
+                const chatsData = [];
+                querySnapshot.forEach((doc) => {
+                    chatsData.push({ id: doc.id, ...doc.data() });
+                });
+                setChats(chatsData);
+            })
+
+            return () => unsubscribe();
+        }
+    }, [auth.currentUser]);
 
     return(
         <aside>
             <h2>My Chats</h2>
+            <ul>
+                {chats.map((chat) => (
+                    <li key={chat.id}>
+                        {chat.title}
+                    </li>
+                ))}
+            </ul>
         </aside>
     );
 }
